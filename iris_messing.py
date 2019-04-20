ce09b6767d21ab00e5b64c986fad3d10dff1a84b
Fintan Hegarty <fintan.hegarty@gmail.com>
2019-04-20 04:54:02 +0100

    bell curve added

--- iris_messing.py
+++ iris_messing.py
@@ -19,8 +19,6 @@ iris_data_set=pandas.read_csv("iris.data")
 # Want to find the standard deviation, median, mode, mean, max, min, 
 # of a given column - maybe ask user to pick? Perhaps too many variables.
 # Want to find the correlation between any two columns - user option.
-# Want to plot some stuff, but what?
-# Do we ask user for that, and if so, ask them for more input upon figure close?
 
 #print(iris_data_set.describe())
 #for i in iris_data_set['class'].unique():
@@ -32,11 +30,11 @@ iris_data_set=pandas.read_csv("iris.data")
 
 print(iris_data_set.head())
 
-for i in iris_data_set['class'].unique():
-    prefiltered=iris_data_set['class']==i
-    filtered = iris_data_set[prefiltered]
-    print('\n',i,'\n\n',filtered.describe(),'\n')
-
+#for i in iris_data_set['class'].unique():
+#    prefiltered=iris_data_set['class']==i
+#    filtered = iris_data_set[prefiltered]
+#    print('\n',i,'\n\n',filtered.describe(),'\n')
+#
 
 #for i in range(1,5):
 #    for j in ['mean','median','mode','std','max','min']:
@@ -54,21 +52,31 @@ for i in iris_data_set['class'].unique():
 #
 
 # To create a map of Ireland
-# >>> sorted_iris=iris_data_set.sort_values(['sepalLength'],ascending=False)
-#>>> for i in range(0,4):
-#...     plt.plot(sorted_iris.iloc[:,i])
-#... 
-# plt.show()
+#sorted_iris=iris_data_set.sort_values(['sepalLength'],ascending=False)
+#for i in range(0,4):
+#    plt.plot(sorted_iris.iloc[:,i]) 
+#
+#plt.show()
 
 #webbrowser.open('https://www.youtube.com/watch?v=NdYWuo9OFAw', new=2)
 
-#import seaborn as sns
-#
+import seaborn as sns
+
 #for i in ['sepalLength','sepalWidth','petalLength','petalWidth']:
 #    sns.boxplot(y=i, x='class', data=iris_data_set) 
 #    sns.despine()
 #    plt.show()
 #
+
+for i in iris_data_set['class'].unique():
+    prefiltered=iris_data_set['class']==i
+    filtered = iris_data_set[prefiltered]
+    for j in ['sepalLength','sepalWidth','petalLength','petalWidth']:
+        title=i
+        plt.title(title)
+        x=filtered[j]
+        sns.distplot(x)
+        plt.show()
 
 
 

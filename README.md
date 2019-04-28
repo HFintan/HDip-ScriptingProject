89e0f2572c456a14752e333fb7536d2766e1c74b
Fintan Hegarty <fintan.hegarty@gmail.com>
2019-04-28 23:32:45 +0100

    tick tock

--- README.md
+++ README.md
@@ -3,4 +3,56 @@ https://github.com/ianmcloughlin/project-pands/blob/master/project.pdf
 
 It centres on the analysis of the Fisher's Iris data set.
 
+The python script iris_messing.py contains all of the various bits of code that were 
+used to produce these data and figures. They are all commented out, and can be run individually,
+so as not to bombard the user. If bombardment is what you're after, the command
+:%s/^#//g
+in vim should uncomment all the necessary parts at once.
+
+In brief, the script labelled 1 returns max, min, std deviation etc.
+
+
+The script 2 returns the correlation. 
+
+The script 3 returns, sequentially, a graph for each species, displaying the plants' four attributes together.
+
+Script 4 isn't very useful, but the output looks like a map of Ireland, so I kept it in out of patriotism.
+
+Script 5 opens a youtube tab with Iris by the GooGoo Dolls. Making it play automatically involves a slightly more invasive script than would probably be appreciated, so the user will have to click the play button themselves.
+
+Script 6 gives box plots comparing the four attributes of the various species.
+
+The script iris.pdf contains a copy of the data and images produced by running the scripts.
+
 If there are any questions, please email me at fintan[dot]hegarty[at]gmail[dot]com.
+
+
+References (alongside those python-specific references listed in comments in iris_messing.py
+
+Wiki page: https://en.wikipedia.org/wiki/Iris_flower_data_set
+
+
+This data set consists of 50 samples of each of three types of iris, all collected under the same conditions (location, time, measurement apparatus and method) to minimise any unexpected variation in the plant samples.
+
+Four features were measured; length of sepals, length of petals, width of sepals, and width of petals. The species was also noted.
+
+
+Findings:
+The mean, standard deviation, min and max etc, for the entire sample set are as follows.
+
+        sepalLength  sepalWidth  petalLength  petalWidth
+mean      5.843333    3.054000     3.758667    1.198667
+std       0.828066    0.433594     1.764420    0.763161
+min       4.300000    2.000000     1.000000    0.100000
+max       7.900000    4.400000     6.900000    2.500000
+
+Further info on the individual species is available in iris.pdf.
+
+Unseparated by class, there appears to be strong correlation between sepal length and petal length, and between sepal length and petal width, and between petal length and petal width. Sepal width, however, seems to be uncorrelated with anything.
+Interestingly, when all the species are taken together, everything appears strongly correlated bar the sepal width, but when the species are taken separately, the correlation is much weaker, which I guess makes sense because the variance, relatively speaking, is smaller. A group of plants comprised of bonzai and a sequoia would exhibit a stronger correlation between branch length and thickness than the sequoia would on their own, for example.
+
+
+
+
+
+
--- iris_messing.py
+++ iris_messing.py
@@ -1,82 +1,110 @@
-# Python project - Fintan Hegarty
-# April 19th 2019
-#  
+## Python project - Fintan Hegarty
+## April 19th 2019
+##  
 
-# References used:
-# https://www.dataquest.io/blog/pandas-python-tutorial/
-# https://stackoverflow.com/questions/31715119/how-can-i-open-a-website-in-my-web-browser-using-python
-# https://www.geeksforgeeks.org/python-pandas-dataframe-sort_values-set-2/
-# https://seaborn.pydata.org/examples/horizontal_boxplot.html
+## References used:
+## https://www.dataquest.io/blog/pandas-python-tutorial/
+## https://stackoverflow.com/questions/31715119/how-can-i-open-a-website-in-my-web-browser-using-python
+## https://www.geeksforgeeks.org/python-pandas-dataframe-sort_values-set-2/
+## https://seaborn.pydata.org/examples/horizontal_boxplot.html
+## https://stackoverflow.com/questions/3253966/python-string-to-attribute
+## https://matplotlib.org/api/_as_gen/matplotlib.pyplot.legend.html
 
 import pandas
 import webbrowser
 import matplotlib.pyplot as plt
 
-# (After remembering to add the header values from iris.names section 7)
+## (After remembering to add the header values from iris.names section 7)
 
 iris_data_set=pandas.read_csv("iris.data")
 
-# Want to find the standard deviation, median, mode, mean, max, min, 
-# of a given column - maybe ask user to pick? Perhaps too many variables.
-# Want to find the correlation between any two columns - user option.
+## Give the standard deviation, median, mode, mean, max, min of a given column.
+## .describe gives most of these, but can also use something like the following:
+
+## 1.
+## for i in range(1,5):
+##   for j in ['median','mode']:
+##        we_want = str(iris_data_set.iloc[i,:].median)
+##        print('The', j, 'of column', i, 'is', we_want, sep=' ')
+## This is giving some weird junk along with the correct answer, so I'll leave it out.
+## It also gives the same weird junk for
+##       print('The median of column', i, 'is', iris_data_set.iloc[i,:].median, sep=' ')
+## so it doesn't appear to be an issue with looping through the list rather than doing
+## each attribute separately
 
 #print(iris_data_set.describe())
-#for i in iris_data_set['class'].unique():
-##    print(i)
-#    filtered_info=iris_data_set[i]
-#    print(filtered_info)
-##    print(filtered_info.describe())
 #
-
-print(iris_data_set.head())
-
 #for i in iris_data_set['class'].unique():
 #    prefiltered=iris_data_set['class']==i
 #    filtered = iris_data_set[prefiltered]
 #    print('\n',i,'\n\n',filtered.describe(),'\n')
 #
 
-#for i in range(1,5):
-#    for j in ['mean','median','mode','std','max','min']:
-#        print(blagh)
-#        echo(print('The', j, 'of column', i, 'is', iris_data_set.iloc[i,:].j, sep=''))
 
-
-# Graphs stuff
-#for i in range(0,4):
-#    plt.plot(iris_data_set.iloc[:,i])
+## 2.
+## Want to find the correlation between any two columns
+    
+#print("Total correlation\n")
+#print(iris_data_set.corr())
 #
-#plt.show()
-#    
-#plt.plot(iris_data_set.iloc[:,2])
-#
+#for i in iris_data_set['class'].unique():
+#    prefiltered=iris_data_set['class']==i
+#    filtered = iris_data_set[prefiltered]
+#    print('\n',i,'\n\n',filtered.corr(),'\n')
 
-# To create a map of Ireland
+
+## 3.
+## Graphs the various values, separated by species.
+
+#attribs=['sepal length','sepal width','petal length','petal width']
+#markers=['o','^','s','x']
+#
+#for i in iris_data_set['class'].unique():
+#    prefiltered=iris_data_set['class']==i
+#    filtered = iris_data_set[prefiltered]
+#    title = i
+#    for j in range(0,4):
+#        plt.plot(filtered.iloc[:,j], label=attribs[j], marker=markers[j])
+#        plt.title(title)
+#        legend= plt.legend(loc = 'upper left')
+#    plt.show()
+## I thought sorting by sepal length or one of the others would make it easier
+## to see correlation etc, but the multiple instances of the same number
+## mean the resulting chart is junk
+##    sorted_iris=iris_data_set.sort_values(['sepalLength'],ascending=True)
+ 
+
+## 4. 
+## To create a surprising map of Ireland
 #sorted_iris=iris_data_set.sort_values(['sepalLength'],ascending=False)
 #for i in range(0,4):
 #    plt.plot(sorted_iris.iloc[:,i]) 
 #
 #plt.show()
 
+## 5.
+## Opens IRIS by the GooGoo Dolls. Can't get it to play automatically without forcing
+## a package download on the user's machine, which would probably be frowned upon.
 #webbrowser.open('https://www.youtube.com/watch?v=NdYWuo9OFAw', new=2)
 
-import seaborn as sns
+## 6.  
+#import seaborn as sns
 
 #for i in ['sepalLength','sepalWidth','petalLength','petalWidth']:
 #    sns.boxplot(y=i, x='class', data=iris_data_set) 
 #    sns.despine()
 #    plt.show()
-#
 
-for i in iris_data_set['class'].unique():
-    prefiltered=iris_data_set['class']==i
-    filtered = iris_data_set[prefiltered]
-    for j in ['sepalLength','sepalWidth','petalLength','petalWidth']:
-        title=i
-        plt.title(title)
-        x=filtered[j]
-        sns.distplot(x)
-        plt.show()
+
+#for i in iris_data_set['class'].unique():
+#    prefiltered=iris_data_set['class']==i
+#    filtered = iris_data_set[prefiltered]
+#    for j in ['sepalLength','sepalWidth','petalLength','petalWidth']:
+#        title=i
+#        plt.title(title)
+#        x=filtered[j]
+#        sns.distplot(x)
+#        plt.show()
 
 
 
